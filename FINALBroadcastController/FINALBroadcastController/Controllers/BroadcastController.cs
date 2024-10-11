bfcd9df8852597727ac823a443c8d2c77986b68e
Nahlan <nahlan-nahla@outlook.com>
2024-10-11 03:44:39 -0400

    Add project files.

---
+++ FINALBroadcastController/FINALBroadcastController/Controllers/BroadcastController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FINALBroadcastLibrary;
+
+
+namespace FINALBroadcastController.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BroadcastController : ControllerBase
+    {
+        private readonly Broadcast _broadcast;
+                                                                            
+        public BroadcastController()
+        {
+            _broadcast = new Broadcast();
+        }
+
+        [HttpGet("getBroadcastMessage")]
+        public IActionResult GetBroadcastMessage()
+        {
+            var message = _broadcast.BroadcastMessage();
+            return Ok(message);
+        }
+    }
+}
---
+++ FINALBroadcastLibrary/FINALBroadcastLibrary/Broadcast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINALBroadcastLibrary
+{
+	public class Broadcast
+	{
+		public string BroadcastMessage()
+		{
+			return "This is a broadcast message!";
+		}
+	}
+}
